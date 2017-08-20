abd9f907cfb6dd8153a3031a6898088df3caceca
Daniel Downes <daniel@doubledaregames.co.uk>
2017-08-20 19:28:19 +0100

    Working but with defects when out of screen-bounds

--- Assets/SetViewportToUiRect.cs
+++ Assets/SetViewportToUiRect.cs
@@ -1,14 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteInEditMode]
+// Note ensure editor resolution matches Canvas fixed resolution (1024 x 768)
+//
+
+/// <summary>
+/// The intention is to allow the viewport to be move around the screen as controlled by a RectTransform, which it does.
+/// 
+/// Howevever there is a stretching distortion effect when the viewport is set to go beyond the screen-bounds.
+/// In which case the camera projection matrix is adjust to compensiate. This is almost perfect when the screen aspect ratio#
+/// is set to 1:1, however there a problems on other resolutions which is prooving problomatic to recify.
+/// 
+/// There is an alternative method to use render textures and simply apply the render texture to a UI imaging, however this
+/// render textures are very memory hungry, especially when considering multiple viewports are involved.
+/// </summary>
 public class SetViewportToUiRect : MonoBehaviour
 {
     public RectTransform UiRect;
+
+    public bool CameraPerspectiveAdjust = false;
+
     private Camera cam;
 
-    public float aspectt = 1;
+    public float aspect = 1;
 
     public float left = -0.2F;
     public float right = 0.2F;
@@ -18,6 +34,9 @@ public class SetViewportToUiRect : MonoBehaviour
     void Start()
     {
         cam = this.GetComponent<Camera>();
+        
+        cam.fieldOfView = (CameraPerspectiveAdjust ? 22.8f : 46.8f);
+        
         DoFit();
     }
 
@@ -28,40 +47,37 @@ public class SetViewportToUiRect : MonoBehaviour
 
     private void DoFit()
     {
-        cam.rect = RectTransformToScreenSpace(UiRect);
+        // Get RectTransform rect in screen-space
+        Rect rectScreen = RectTransformToScreenSpace(UiRect);
 
-        //Rect offset = new Rect( )
+        cam.rect = new Rect(rectScreen.x / Screen.width,
+                            rectScreen.y / Screen.height,
+                            rectScreen.width / Screen.width,
+                            rectScreen.height / Screen.height);
 
-        // Compensate ratio scaling when viewport off-screen
-        float offsetBottom = (cam.rect.y / Screen.height);
-        if (offsetBottom > 0)
-            offsetBottom = 0;
+        // Apply camera perspective off-setting when viewport going beyond the screen-bounds
+        if (CameraPerspectiveAdjust)
+        {
+            float offsetBottom = (rectScreen.y) / Screen.height;
+            if (offsetBottom > 0)
+                offsetBottom = 0;
+            float offsetTop = ((rectScreen.y + rectScreen.height) / Screen.height) - 1;
+            if (offsetTop < 0)
+                offsetTop = 0;
+            float offsetLeft = (rectScreen.x / Screen.width);
+            if (offsetLeft > 0)
+                offsetLeft = 0;
+            float offsetRight = ((rectScreen.x + rectScreen.width) / Screen.width) - 1;
+            if (offsetRight < 0)
+                offsetRight = 0;
 
-        float offsetTop = ((cam.rect.y + cam.rect.height) / Screen.height) - 1;
-        if (offsetTop < 0)
-            offsetTop = 0;
-
-        float offsetLeft = (cam.rect.x + cam.rect.width) / Screen.width;
-        if (offsetLeft > 0)
-            offsetLeft = 0;
-
-        float offsetRight = ((cam.rect.x + cam.rect.width) / Screen.width) - 1;
-        if (offsetRight < 0)
-            offsetRight = 0;
-
-        //TODO: When rect over half off screen, Perspective should start to follow centre of rect
-        //    
-        cam.rect = new Rect(cam.rect.x / Screen.width,
-            cam.rect.y / Screen.height,
-            cam.rect.width / Screen.width,
-            cam.rect.height / Screen.height);
-        
-        Matrix4x4 m = PerspectiveOffCenter(left - offsetLeft, right - offsetRight, bottom - offsetBottom, top - offsetTop, cam.nearClipPlane, cam.farClipPlane);
-        cam.projectionMatrix = m;
+            Matrix4x4 m = PerspectiveOffCenter(left - offsetLeft, right - offsetRight, bottom - offsetBottom, top - offsetTop, cam.nearClipPlane, cam.farClipPlane);
+            cam.projectionMatrix = m;
+        }
     }
 
     // Thanks: http://answers.unity3d.com/questions/1013011/convert-recttransform-rect-to-screen-space.html
-    public static Rect RectTransformToScreenSpace(RectTransform transform)
+    private static Rect RectTransformToScreenSpace(RectTransform transform)
     {
         Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
         Rect rect = new Rect(transform.position.x, transform.position.y, size.x, size.y);
@@ -72,7 +88,7 @@ public class SetViewportToUiRect : MonoBehaviour
     }
 
     // https://docs.unity3d.com/ScriptReference/Camera-projectionMatrix.html
-    static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
+    private static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
     {
         float x = 2.0F * near / (right - left);
         float y = 2.0F * near / (top - bottom);
